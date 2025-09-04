c99285dad16b3f06a2b621cc576ffe44f263409b
thatshowmafiaworks <shinehd56@gmail.com>
2025-09-04 18:20:43 +0300

    added two fields to user model, updated db

--- TaskManagementApi/Models/AppUser.cs
+++ TaskManagementApi/Models/AppUser.cs
@@ -3,4 +3,6 @@
 public class AppUser : IdentityUser<Guid>
 {
     public List<Task> Tasks { get; set; } = new List<Task>();
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
