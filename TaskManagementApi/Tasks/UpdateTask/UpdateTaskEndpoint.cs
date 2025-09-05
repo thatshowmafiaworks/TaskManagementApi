a3edff93363e966a0d70e0664bc7734e93fc5d44
thatshowmafiaworks <shinehd56@gmail.com>
2025-09-05 20:05:26 +0300

    implemented update task feature

---
+++ TaskManagementApi/Tasks/UpdateTask/UpdateTaskEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagementApi.Tasks.UpdateTask;
+
+public record UpdateTaskRequest(
+    string? Title = null,
+    string? Description = null,
+    DateTime? DueDate = null,
+    Status? Status = null,
+    Priority? Priority = null
+    );
+public record UpdateTaskResponse(bool IsSuccess, string? Error = null);
+
+public class UpdateTaskEndpoint
+    : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/tasks/{Id}", async (Guid Id, [FromBody] UpdateTaskRequest request, HttpContext context, ISender sender) =>
+        {
+            var userId = Guid.Parse(context.User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var command = new UpdateTaskCommand(
+                userId,
+                Id,
+                request.Title,
+                request.Description,
+                request.DueDate,
+                request.Status,
+                request.Priority);
+
+            var result = await sender.Send(command);
+            var response = result.Adapt<UpdateTaskResponse>();
+
+            return Results.Ok(response);
+        })
+            .RequireAuthorization();
+    }
+}
---
+++ TaskManagementApi/Tasks/UpdateTask/UpdateTaskHandler.cs
@@ -0,0 +1,37 @@
+﻿namespace TaskManagementApi.Tasks.UpdateTask;
+
+public record UpdateTaskCommand(
+    Guid UserId,
+    Guid Id,
+    string? Title = null,
+    string? Description = null,
+    DateTime? DueDate = null,
+    Status? Status = null,
+    Priority? Priority = null
+    ) : ICommand<UpdateTaskResult>;
+public record UpdateTaskResult(bool IsSuccess, string? Error = null);
+
+public class UpdateTaskHandler(TasksRepository tasksRepo
+    )
+    : ICommandHandler<UpdateTaskCommand, UpdateTaskResult>
+{
+    public async Task<UpdateTaskResult> Handle(UpdateTaskCommand command, CancellationToken cancellationToken)
+    {
+        var task = await tasksRepo.Get(command.Id);
+        if (command.UserId != task.UserId)
+            return new UpdateTaskResult(false, "Forbiden");
+        if (command.Title != null)
+            task.Title = command.Title;
+        if (command.Description != null)
+            task.Description = command.Description;
+        if (command.DueDate != null)
+            task.DueDate = command.DueDate;
+        if (command.Status.HasValue)
+            task.Status = command.Status.Value;
+        if (command.Priority.HasValue)
+            task.Priority = command.Priority.Value;
+        task.UpdatedAt = DateTime.UtcNow;
+        await tasksRepo.Update(task);
+        return new UpdateTaskResult(true);
+    }
+}
