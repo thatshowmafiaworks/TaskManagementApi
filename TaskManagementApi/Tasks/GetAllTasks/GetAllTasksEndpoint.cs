af8cbf26eeb749ae607e5e39443fb7972db7d5bd
thatshowmafiaworks <shinehd56@gmail.com>
2025-09-05 16:42:21 +0300

    implemented GetAllTasks feature

---
+++ TaskManagementApi/Tasks/GetAllTasks/GetAllTasksEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+namespace TaskManagementApi.Tasks.GetAllTasks;
+
+public record GetAllTasksRequest(
+    int? PageNumber = 1,
+    int? PageSize = 10,
+    Status? Status = null,
+    Priority? Priority = null);
+public record GetAllTasksResponse(IEnumerable<Models.Task> Tasks);
+
+public class GetAllTasksEndpoint
+    : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/tasks",
+            async ([AsParameters] GetAllTasksRequest request, ISender sender) =>
+        {
+            var query = request.Adapt<GetAllTasksQuery>();
+            var result = await sender.Send(query);
+            var response = result.Adapt<GetAllTasksResponse>();
+            return Results.Ok(response);
+        })
+           .RequireAuthorization();
+    }
+}
---
+++ TaskManagementApi/Tasks/GetAllTasks/GetAllTasksHandler.cs
@@ -0,0 +1,24 @@
+﻿
+namespace TaskManagementApi.Tasks.GetAllTasks;
+
+public record GetAllTasksQuery(
+    int? PageNumber = 1,
+    int? PageSize = 10,
+    Status? Status = null,
+    Priority? Priority = null) : IQuery<GetAllTasksResult>;
+public record GetAllTasksResult(IEnumerable<Models.Task> Tasks);
+
+public class GetAllTasksHandler(
+    TasksRepository taskRepo)
+    : IQueryHandler<GetAllTasksQuery, GetAllTasksResult>
+{
+    public async Task<GetAllTasksResult> Handle(GetAllTasksQuery query, CancellationToken cancellationToken)
+    {
+        var tasks = await taskRepo.GetAll(
+            query.PageNumber ?? 1,
+            query.PageSize ?? 10,
+            query.Status,
+            query.Priority);
+        return new GetAllTasksResult(tasks);
+    }
+}
