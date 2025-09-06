9855aa74bb1eba76ddfc96f985541fc08f81df36
thatshowmafiaworks <shinehd56@gmail.com>
2025-09-06 14:10:13 +0300

    implemented delete task feature

---
+++ TaskManagementApi/Tasks/DeleteTask/DeleteTaskEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskManagementApi.Tasks.DeleteTask;
+
+//public record DeleteTaskRequest();
+public record DeleteTaskResponse(bool IsSuccess, string? Error = null);
+
+public class DeleteTaskEndpoint
+    : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/tasks/{Id}", async (Guid Id, ISender sender, HttpContext context) =>
+        {
+            var userId = Guid.Parse(context.User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var command = new DeleteTaskCommand(Id, userId);
+            var result = await sender.Send(command);
+            var response = result.Adapt<DeleteTaskResult>();
+            return Results.Ok(response);
+        })
+            .RequireAuthorization();
+    }
+}
---
+++ TaskManagementApi/Tasks/DeleteTask/DeleteTaskHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace TaskManagementApi.Tasks.DeleteTask;
+public record DeleteTaskCommand(Guid Id, Guid UserId) : ICommand<DeleteTaskResult>;
+public record DeleteTaskResult(bool IsSuccess, string? Error = null);
+
+public class DeleteTaskHandler(TasksRepository tasksRepo)
+    : ICommandHandler<DeleteTaskCommand, DeleteTaskResult>
+{
+    public async Task<DeleteTaskResult> Handle(DeleteTaskCommand command, CancellationToken cancellationToken)
+    {
+        var task = await tasksRepo.Get(command.Id);
+        if (task.UserId != command.UserId)
+            return new DeleteTaskResult(false, "Forbiden");
+        await tasksRepo.Delete(task);
+        return new DeleteTaskResult(true);
+    }
+}
